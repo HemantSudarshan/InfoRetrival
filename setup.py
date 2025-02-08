f9a978b13cc6d9782a94b8de41e3952a195636f3
Hemant Sudarshan <141361950+HemantSudarshan@users.noreply.github.com>
2025-02-09 03:57:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'Bappy',
-    author_email= 'boktiar@ineuron.ai',
+    author= 'Hemant',
+    author_email= 'Hemanthsudarshan2002@gmail.com',
     packages= find_packages(),
     install_requires = []
 
